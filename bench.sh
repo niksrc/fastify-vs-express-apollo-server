86184d59124ed69ea19ea43f4966d2075ed84e7c
Nikhil Srivastava <niksrc@gmail.com>
2019-11-19 17:54:06 +0530

    add basic bench mark

---
+++ bench.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Fastify Hello World Run
+npx autocannon http://127.0.0.1:3000/graphql -m POST -b '{"query":"{\n  hello\n}\n"}' -H "Accept:application/json" -H "Content-Type:application/json" --no-progress
+
+echo Express Hello World Run
+npx autocannon http://127.0.0.1:4000/graphql -m POST -b '{"query":"{\n  hello\n}\n"}' -H "Accept:application/json" -H "Content-Type:application/json" --no-progress 
